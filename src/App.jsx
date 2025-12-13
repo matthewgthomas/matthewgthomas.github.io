1dc3085c7cb594eec46db8ab134786383a07ef7e
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-12-13 11:08:20 +0000

    Remove unused component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Writing from './pages/Writing';
 import Research from './pages/Research';
